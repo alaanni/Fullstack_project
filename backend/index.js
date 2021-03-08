0fc23f39ddb4ce72a5766e025b5287a07d7bd5fc
Ala-Mattinen Anni <alaanni@cubbli20-gold.local>
2021-03-08 21:19:56 +0200

    depoloying

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
+const PORT = config.PORT || 3001
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
