722f13c3853908d0a8f14a5bd7a872bc19e27860
Ala-Mattinen Anni <alaanni@cubbli20-gold.local>
2021-03-09 12:02:00 +0200

    deploy

---
+++ react-ui/build/precache-manifest.f4e5b2d01580fa3847cf062d9f8896d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2946087e882174e1b7548e9ec804a284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0e607ee2459ee8b03a2",
+    "url": "/static/css/main.dc6796d0.chunk.css"
+  },
+  {
+    "revision": "0d2506f3fc883ac2ccda",
+    "url": "/static/js/2.6c618424.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6c618424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e607ee2459ee8b03a2",
+    "url": "/static/js/main.636e32aa.chunk.js"
+  },
+  {
+    "revision": "041621c0757848d23643",
+    "url": "/static/js/runtime-main.89ce72de.js"
+  }
+]);
